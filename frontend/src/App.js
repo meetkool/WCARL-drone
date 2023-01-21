fcb6b59a9ff0ac771fb45df7ca2c73e1f4f5a026
KarthikS373 <lit2021012@iiitl.ac.in>
2023-01-21 19:34:39 +0530

    Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+import HomePage from "./pages/home.page"
+import CheckmyDevicePage from "./pages/checkmyDevice.page"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/check-my-device",
+    element: <CheckmyDevicePage />,
+  },
+])
+
 const App = () => {
-  return <div className="App"></div>
+  return <RouterProvider router={router} />
 }
 
 export default App
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client"
 
 import App from "./App"
 
-import "./index.css"
+import "bootstrap/dist/css/bootstrap.css"
+import "./style/global.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
