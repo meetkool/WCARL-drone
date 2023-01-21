df7fe297c3143c07fd686970470781059551258e
KarthikS373 <lit2021012@iiitl.ac.in>
2023-01-21 20:31:01 +0530

    fixed popper js isssue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import App from "./App"
 import "bootstrap/dist/css/bootstrap.css"
 import "./style/global.css"
 
+import "bootstrap/dist/js/bootstrap.bundle"
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
