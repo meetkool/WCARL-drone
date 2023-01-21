cc2225326e7e2671d80215e17db6610646698370
aadi58002 <aadi58002gmail.com>
2023-01-21 22:50:28 +0530

    Adding component in the App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,15 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import CheckmyDevicePage from "./pages/checkmyDevice.page"
 import HomePage from "./pages/home.page"
 
+
+// Sensors
+import AccelerometerPage from "./pages/sensors/AccelerometerPage"
+import CameraPage from "./pages/sensors/CameraPage"
+import GyroscopePage from "./pages/sensors/GyroscopePage"
+import NetworkPage from "./pages/sensors/NetworkPage"
+import ProximityPage from "./pages/sensors/ProximityPage"
+import StoragePage from "./pages/sensors/StoragePage"
+
 const router = createBrowserRouter([
   {
     path: "/",
