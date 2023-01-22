7669a6de2839f5913409ab9910d1edc01fc1b6b8
aadi58002 <aadi58002gmail.com>
2023-01-22 12:19:07 +0530

    Fixing chartjs and adding some scripts

--- frontend/src/pages/sensors/AccelerometerPage.jsx
+++ frontend/src/pages/sensors/AccelerometerPage.jsx
@@ -3,7 +3,7 @@ import Layout from "../../components/layout/Layout.component"
 import Loader from "../../components/Loader";
 import { checkSensors, checkMediaSupport } from "../../utils/check-sensors.js";
 import { Round } from "../../utils/utils.js";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Chart, registerables } from 'chart.js';
 const AccelerometerPage = () => {
     let [accelerometerStat, setAccelerometerStat] = useState(false);
@@ -21,106 +21,103 @@ const AccelerometerPage = () => {
         const DATA_COUNT = 50;
         const frequency = 10;
 
-        if (typeof window !== 'undefined') {
-            // draw chart
-            Chart.register(...registerables);
-            const ctx = document.getElementById('myChart').getContext('2d');
+        // draw chart
+        Chart.register(...registerables);
+        const ctx = document.getElementById('myChart').getContext('2d');
 
-            const labels = [];
-            for (let i = 0; i <= DATA_COUNT; ++i) {
-                labels.push(i.toString());
-            }
-            const data = {
-                labels: labels,
-                datasets: [
-                    {
-                        label: 'Acceleration X',
-                        data: [],
-                        borderColor: '#dc3545',
-                        backgroundColor: '#dc3545aa',
-                        fill: false,
-                        cubicInterpolationMode: 'monotone',
-                        tension: 0.4,
-                    },
-                    {
-                        label: 'Acceleration Y',
-                        data: [],
-                        borderColor: '#28a745',
-                        backgroundColor: '#28a745aa',
-                        fill: false,
-                        cubicInterpolationMode: 'monotone',
-                        tension: 0.4,
-                    },
-                    {
-                        label: 'Acceleration Z',
-                        data: [],
-                        borderColor: '#007bff',
-                        backgroundColor: '#007bffaa',
-                        fill: false,
-                        cubicInterpolationMode: 'monotone',
-                        tension: 0.4,
-                    },
-                ]
-            };
+        const labels = [];
+        for (let i = 0; i <= DATA_COUNT; ++i) {
+            labels.push(i.toString());
+        }
+        const data = {
+            labels: labels,
+            datasets: [
+                {
+                    label: 'Acceleration X',
+                    data: [],
+                    borderColor: '#dc3545',
+                    backgroundColor: '#dc3545aa',
+                    fill: false,
+                    cubicInterpolationMode: 'monotone',
+                    tension: 0.4,
+                },
+                {
+                    label: 'Acceleration Y',
+                    data: [],
+                    borderColor: '#28a745',
+                    backgroundColor: '#28a745aa',
+                    fill: false,
+                    cubicInterpolationMode: 'monotone',
+                    tension: 0.4,
+                },
+                {
+                    label: 'Acceleration Z',
+                    data: [],
+                    borderColor: '#007bff',
+                    backgroundColor: '#007bffaa',
+                    fill: false,
+                    cubicInterpolationMode: 'monotone',
+                    tension: 0.4,
+                },
+            ]
+        };
 
-            const config = {
-                type: 'line',
-                data: data,
-                options: {
-                    responsive: false,
-                    plugins: {
+        const config = {
+            type: 'line',
+            data: data,
+            options: {
+                responsive: false,
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Linear Acceleration Sensor'
+                    },
+                },
+                interaction: {
+                    intersect: false,
+                },
+                scales: {
+                    x: {
+                        display: false,
+                        title: {
+                            display: false,
+                        }
+                    },
+                    y: {
+                        display: true,
                         title: {
                             display: true,
-                            text: 'Linear Acceleration Sensor'
+                            text: 'Value'
                         },
-                    },
-                    interaction: {
-                        intersect: false,
-                    },
-                    scales: {
-                        x: {
-                            display: false,
-                            title: {
-                                display: false,
-                            }
-                        },
-                        y: {
-                            display: true,
-                            title: {
-                                display: true,
-                                text: 'Value'
-                            },
-                            suggestedMin: -10,
-                            suggestedMax: 10
-                        }
-                    },
-                    elements: {
-                        point: {
-                            radius: 0
-                        }
+                        suggestedMin: -10,
+                        suggestedMax: 10
                     }
                 },
-            };
-            if (lineChart != undefined)
-            {lineChart.destroy();}
-            lineChart = new Chart(ctx, config);
+                elements: {
+                    point: {
+                        radius: 0
+                    }
+                }
+            },
+        };
+        if (lineChart != undefined) { lineChart.destroy(); }
+        lineChart = new Chart(ctx, config);
 
-            // // For debugging on PC if sensor is not available
-            // let counter = 0;
-            // setInterval(() => {
-            //   // order matters
-            //   // first splice -> then push
-            //   if (counter > DATA_COUNT) {
-            //     lineChart.data.labels.splice(0, 1);
-            //     lineChart.data.labels.push(counter.toString());
-            //     lineChart.data.datasets[0].data.splice(0, 1);
-            //   }
+        // // For debugging on PC if sensor is not available
+        // let counter = 0;
+        // setInterval(() => {
+        //   // order matters
+        //   // first splice -> then push
+        //   if (counter > DATA_COUNT) {
+        //     lineChart.data.labels.splice(0, 1);
+        //     lineChart.data.labels.push(counter.toString());
+        //     lineChart.data.datasets[0].data.splice(0, 1);
+        //   }
 
-            //   lineChart.data.datasets[0].data.push(Math.random() * 2 - 1);
-            //   counter++;
-            //   lineChart.update();
-            // }, 100);
-        }
+        //   lineChart.data.datasets[0].data.push(Math.random() * 2 - 1);
+        //   counter++;
+        //   lineChart.update();
+        // }, 100);
 
         let accelerometer = null;
         try {
@@ -170,7 +167,7 @@ const AccelerometerPage = () => {
             <div className="container-xxl">
                 <div className="fw-bold text-uppercase mb-8 text-center mt-4 fs-1">Accelerometer</div>
                 <div className="container text-center">
-                    <Link to="/sensors/accelerometer" type="button" className="btn btn-outline-success mb-5 btn-lg" style={{width: "150px", height: "50px"}}>Next</Link>
+                    <Link to="/sensors/gyroscope" type="button" className="btn btn-outline-success mb-5 btn-lg" style={{ width: "150px", height: "50px" }}>Next</Link>
                 </div>
             </div>
             <div>
--- frontend/src/pages/sensors/GyroscopePage.jsx
+++ frontend/src/pages/sensors/GyroscopePage.jsx
@@ -1,11 +1,175 @@
-import React from "react"
+import React, { useEffect } from 'react'
 import Layout from "../../components/layout/Layout.component"
-const ProximityPage = () => {
-    return (
+import Loader from "../../components/Loader";
+import { checkSensors, checkMediaSupport } from "../../utils/check-sensors.js";
+import { Round } from "../../utils/utils.js";
+import {Link} from "react-router-dom";
+import { Chart, registerables } from 'chart.js';
+
+const GyroscopeComponent = () => {
+
+  useEffect(() => {
+
+    let lineChart;
+    const DATA_COUNT = 50;
+
+    if (typeof window !== 'undefined') {
+      // draw chart
+      Chart.register(...registerables);
+      const ctx = document.getElementById('myChart').getContext('2d');
+
+      const labels = [];
+      for (let i = 0; i <= DATA_COUNT; ++i) {
+        labels.push(i.toString());
+      }
+      const data = {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Rotation X',
+            data: [],
+            borderColor: '#dc3545',
+            backgroundColor: '#dc3545aa',
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            tension: 0.4,
+          },
+          {
+            label: 'Rotation Y',
+            data: [],
+            borderColor: '#28a745',
+            backgroundColor: '#28a745aa',
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            tension: 0.4,
+          },
+          {
+            label: 'Rotation Z',
+            data: [],
+            borderColor: '#007bff',
+            backgroundColor: '#007bffaa',
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            tension: 0.4,
+          },
+        ]
+      };
+
+      const config = {
+        type: 'line',
+        data: data,
+        options: {
+          responsive: false,
+          plugins: {
+            title: {
+              display: true,
+              text: 'Gyroscope Sensor'
+            },
+          },
+          interaction: {
+            intersect: false,
+          },
+          scales: {
+            x: {
+              display: false,
+              title: {
+                display: false,
+              }
+            },
+            y: {
+              display: true,
+              title: {
+                display: true,
+                text: 'Value'
+              },
+              suggestedMin: -90,
+              suggestedMax: 90
+            }
+          },
+          elements: {
+            point: {
+              radius: 0
+            }
+          }
+        },
+        data
+      };
+      lineChart = new Chart(ctx, config);
+
+      // // For debugging on PC if sensor is not available
+      // let counter = 0;
+      // setInterval(() => {
+      //   // order matters
+      //   // first splice -> then push
+      //   if (counter > DATA_COUNT) {
+      //     lineChart.data.labels.splice(0, 1);
+      //     lineChart.data.labels.push(counter.toString());
+      //     lineChart.data.datasets[0].data.splice(0, 1);
+      //   }
+
+      //   lineChart.data.datasets[0].data.push(Math.random() * 2 - 1);
+      //   counter++;
+      //   lineChart.update();
+      // }, 100);
+    }
+
+    let gyroscope = null;
+    try {
+      gyroscope = new Gyroscope();
+      gyroscope.onerror = (event) => {
+        // Handle runtime errors.
+        if (event.error.name === 'NotAllowedError') {
+          alert('Permission to access sensor was denied.');
+        } else if (event.error.name === 'NotReadableError') {
+          alert('Cannot connect to the sensor.');
+        }
+      };
+
+      let counter = 0;
+      gyroscope.onreading = (e) => {
+
+        if (counter > DATA_COUNT) {
+          lineChart.data.labels.splice(0, 1);
+          lineChart.data.labels.push(counter.toString());
+          lineChart.data.datasets[0].data.splice(0, 1);
+          lineChart.data.datasets[1].data.splice(0, 1);
+          lineChart.data.datasets[2].data.splice(0, 1);
+        }
+        lineChart.data.datasets[0].data.push(gyroscope.x * 180 / Math.PI);
+        lineChart.data.datasets[1].data.push(gyroscope.y * 180 / Math.PI);
+        lineChart.data.datasets[2].data.push(gyroscope.z * 180 / Math.PI);
+
+        counter++;
+        lineChart.update();
+
+      };
+      gyroscope.start();
+    } catch (error) {
+      // Handle construction errors.
+      if (error.name === 'SecurityError') {
+        alert('Sensor construction was blocked by the Permissions Policy.');
+      } else if (error.name === 'ReferenceError') {
+        alert('Sensor is not supported by the User Agent.');
+      } else {
+        throw error;
+      }
+    }
+    console.log(gyroscope)
+  }, [])
+
+  return (
         <Layout>
-            <div>ProximityPage</div>
+            <div className="container-xxl">
+                <div className="fw-bold text-uppercase mb-8 text-center mt-4 fs-1">Gyroscope</div>
+                <div className="container text-center">
+                    <Link to="/sensors/proximity" type="button" className="btn btn-outline-success mb-5 btn-lg" style={{ width: "150px", height: "50px" }}>Next</Link>
+                </div>
+            </div>
+            <div>
+                <canvas id="myChart" width={350} height={350}></canvas>
+            </div>
         </Layout>
-    )
+  )
 }
 
-export default ProximityPage;
+export default GyroscopeComponent
